00c9c12f09a41f7d9de441b10a5dbb37b465751e
Travis Mix <travismix1980@gmail.com>
2023-09-03 00:57:21 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+namespace HelloRaptors
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            app.UseRouting();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.Run();
+        }
+    }
+}
